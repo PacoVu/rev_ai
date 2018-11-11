bb5c15789a45741f7ab907ba10090601c823e8fd
PacoVu <paco_vu@yahoo.com>
2018-11-11 10:40:53 -0800

    Test release
    - Complete implementation

--- test.js
+++ test.js
@@ -1,64 +1,80 @@
-var revai = require('rev_ai')
-//var rev_ai = require('./lib/revaineedle')
-var apikey = "01rOR8fuc7Rl_tc_4_KucEyh5pem8m55hz-f0VPGMbzuZXFRLdGi8icA8Yf7QkvERLGdtNHxFsZEgM_WTTg-FobSFnYJM"
+//var revai = require('rev_ai')
+require('dotenv').load()
+var rev_ai = require('./lib/revaineedle')
+var apikey = process.env.REVAI_KEY
 var client = new rev_ai.REVAIClient(apikey, "v1beta", null)
 
 var callback = function(err,resp,body){
-  console.log("callback")
+  //console.log(body.toString('utf8'))
+  var json = JSON.parse(body)
+  var transcript = ""
+  for (var item of json.monologues){
+    for (var element of item.elements){
+      transcript += element.value
+    }
+  }
+  console.log("TRANSCRIPT: " + transcript)
+}
+var params = {
+  media: "sample1.mp4",
+  metadata: "This is a sample submit jobs option"
+}
+// callback_url: for webhook
+//id=492373908 // 492373908
+/*
+client.post('jobs', params, function(err,resp,body){
+  console.log(body)
+})
+*/
+var query = 'jobs/' + "53261293/transcript"
+//client.get(query, "", callback)
+
+/*
+var params = {
+  limit: 2
+}
+client.getJobs(params, function(err,res,body){
+  //console.log(body)
+  //var json = JSON.parse(body.toString('utf8'))
+  for (var job of body){
+    console.log(job.status)
+    console.log(job.id)
+    console.log(job.completed_on)
+  }
+})
+*/
+
+
+var id=53261293 // 492373908
+client.getJobById(id, function(err,res,body){
+  console.log(body)
+  //var json = JSON.parse(body.toString('utf8'))
+  if (body.status == "transcribed"){
+    client.getTranscription(body.id, callback)
+  }
+})
+
+/*
+var params = {
+  media: "sample1.mp3",
+  metadata: "This is a sample submit file jobs option"
+}
+client.transcribe(params, function(err,resp,body){
+  console.log(body)
   //console.log(resp)
   //console.log(JSON.stringify( resp.body.toString('utf8')));
   //body.setEncoding('utf8');
   //console.log(body)
-  var json = JSON.parse(resp.body.toString('utf8'))
-  //console.log(resp.body.toString('utf8'))
-  //console.log(json.monologues)
-  var transcript = ""
-  var conversations = []
-  var wordswithoffsets = []
-  var blockTimeStamp = []
-  var sentencesForSentiment = []
-  for (var item of json.monologues){
-    var speakerSentence = {}
-    speakerSentence['sentence'] = []
-    speakerSentence['timestamp'] = []
-    speakerSentence['speakerId'] = item.speaker
-    var sentence = ""
-    for (var element of item.elements){
-      sentence += element.value
-      if (element.type == 'text'){
-        //var wordoffset = {}
-        //wordoffset['word'] = element.value
-        //wordoffset['offset'] = element.ts
-        //wordswithoffsets.push(wordoffset)
-        speakerSentence['timestamp'].push(element.ts)
-        //speakerSentence['sentence'].push(element.value)
-      }
-    }
-    sentence = sentence.trim()
-    transcript += sentence
-    var wordArr = sentence.split(" ")
-    speakerSentence['sentence'] = wordArr
-    //console.log("words #: " + wordArr.length)
-    //console.log("times #: " + speakerSentence.timestamp.length)
-    for (var i=0; i<speakerSentence.timestamp.length; i++){
-      var wordoffset = {}
-      if (i < wordArr.length)
-      wordoffset['word'] = wordArr[i]
-      wordoffset['offset'] = speakerSentence.timestamp[i]
-      wordswithoffsets.push(wordoffset)
-    }
-    conversations.push(speakerSentence)
-  }
-  console.log(transcript)
-  console.log(JSON.stringify(wordswithoffsets))
-  console.log(JSON.stringify(conversations))
-}
-var data = {
-  media_url: "http://www.qcalendar.com/audios/CallhandlingSkills.mp3",
-  metadata: "This is a sample submit jobs option"
-}
-// callback_url: for webhook
-id=492373908 // 492373908
-//client.post('jobs', data, callback)
-var query = 'jobs/' + "492373908/transcript"
-client.get(query, "", callback)
+  //var json = JSON.parse(body.toString('utf8'))
+})
+*/
+/*
+client.get("account", "", function(err,resp,body){
+  console.log(body)
+})
+*/
+/*
+client.account((err,resp,body) => {
+  console.log(body.balance_seconds/60)
+})
+*/
